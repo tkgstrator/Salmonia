11e41651066e89c5e0f42bbd7d809fefe5a3b34e
浅野和奈 <crossguitar@live.jp>
2019-08-23 20:50:39 +0900

    Auto Generating iksm_session and session_token.

--- Salmonia.py
+++ Salmonia.py
@@ -4,33 +4,66 @@ import json
 import os
 from datetime import datetime
 from time import sleep
+import iksm
+
+VERSION = "1.5.0"
+LANG = "en-US"
+
 
 class SalmonRec():
     def __init__(self):
-        print("Salmonia version b1")
-        print("Thanks @Yukinkling and @barley_ural!")
+        print(datetime.now().strftime("%H:%M:%S ") + "Salmonia version b1")
+        print(datetime.now().strftime("%H:%M:%S ") + "Thanks @Yukinkling and @barley_ural!")
         path = os.path.dirname(os.path.abspath(sys.argv[0])) + "/config.json"
         try:
             with open(path) as f:
                 df = json.load(f)
-                self.session = df["iksm_session"]
+                self.cookie = df["iksm_session"]
+                self.session = df["session_token"]
                 self.token = df["api-token"]
                 self.latest = df["latest"]
         except FileNotFoundError:
-            print("config.json is not found.")
-            sys.exit()
+            print(datetime.now().strftime("%H:%M:%S ") + "config.json is not found.")
+            self.setConfig()
 
         url = "https://app.splatoon2.nintendo.net"
-        print("Checking iksm_session's validation.")
-        res = requests.get(url, cookies=dict(iksm_session=self.session))
+        print(datetime.now().strftime("%H:%M:%S ") + "Checking iksm_session's validation.")
+        res = requests.get(url, cookies=dict(iksm_session=self.cookie))
         if res.status_code == 200:
-            print("Your iksm_session is valid.")
+            print(datetime.now().strftime("%H:%M:%S ") + "Your iksm_session is valid.")
         if res.status_code == 403:
             if res.text == "Forbidden":
-                print("Your iksm_session is invalid.")
+                print(datetime.now().strftime("%H:%M:%S ") + "Your iksm_session is expired.")
+                if self.session != "":
+                    print(datetime.now().strftime("%H:%M:%S ") + "Regenerate the iksm_session.")
+                    # Regenerate iksm_session with session_token
+                    cookie = iksm.get_cookie(self.session, LANG, VERSION)
+                    with open("config.json", mode="w") as f:
+                        data = {
+                            "iksm_session": cookie,
+                            "session_token": self.session,
+                            "api-token": self.token,
+                            "latest": self.latest
+                        }
+                        json.dump(data, f, indent=4)
+                else:
+                    self.setConfig()
             else:
-                print("Unknown error.")
+                print(datetime.now().strftime("%H:%M:%S ") + "Unknown error.")
             sys.exit()
+
+    def setConfig(self):
+        token = iksm.log_in(VERSION)
+        cookie = iksm.get_cookie(token, LANG, VERSION)
+        with open("config.json", mode="w") as f:
+            data = {
+                "iksm_session": cookie,
+                "session_token": token,
+                "api-token": "",
+                "latest": 0
+            }
+            json.dump(data, f, indent=4)
+        self.cookie = cookie
 
     def upload(self, path, jobid):
         file = json.load(open(path, "r"))
@@ -39,11 +72,11 @@ class SalmonRec():
         headers = {"Content-type": "application/json",
                    "Authorization": "Bearer " + self.token}
         res = requests.post(url, data=json.dumps(result), headers=headers)
-        
-        if res.status_code == 401: #認証エラー
+
+        if res.status_code == 401:  # 認証エラー
             print("Api token is invalid.")
             sys.exit()
-        if res.status_code == 200: #認証成功
+        if res.status_code == 200:  # 認証成功
             return True
         return False
 
@@ -54,56 +87,62 @@ class SalmonRec():
         for p in dir:
             file.append(p[0:-5])
         file.sort(key=int, reverse=True)
-        
+
         # jobidでアップロード
         for jobid in file:
             # 設定ファイルから読み込んだ最新リザルトIDを使用
             if self.latest >= int(jobid):
-                print(datetime.now().strftime('%Y/%m/%d %H:%M:%S') + " : No new records.")
+                print(datetime.now().strftime(
+                    "%H:%M:%S ") + "No new records.")
                 break
             path = "json/" + jobid + ".json"
             if self.upload(path, jobid) == True:
-                print(datetime.now().strftime('%Y/%m/%d %H:%M:%S') + " : " + jobid + " uploaded success!")
+                print(datetime.now().strftime(" %H:%M:%S ") + jobid + " uploaded!")
             sleep(1)
-            
+
         # 最新リザルトIDを更新
         self.latest = latest
         path = os.path.dirname(os.path.abspath(sys.argv[0])) + "/config.json"
         with open(path, "w") as f:
-            config = {"iksm_session" : self.session, "api-token" : self.token, "latest" : self.latest}
+            config = {"iksm_session": self.cookie,
+                      "api-token": self.token, "latest": self.latest}
             json.dump(config, f, indent=4)
 
     def getResults(self):
         # 最新のリザルトIDを取得する
         url = "https://app.splatoon2.nintendo.net/api/coop_results"
-        res = requests.get(url, cookies=dict(iksm_session=self.session)).json()
+        res = requests.get(url, cookies=dict(iksm_session=self.cookie)).json()
         resid = int(res["summary"]["card"]["job_num"])
         max = 50 if resid >= 50 else resid
 
         dir = os.listdir()
         if "json" not in dir:
-            print("Make Directory...")
+            print(datetime.now().strftime("%H:%M:%S ") + "Make Directory...")
             os.mkdir("json")
         list = os.listdir("json")
         for i in range(0, max):
             if (str(resid - i) + ".json") in list:
                 break
-            print(datetime.now().strftime('%Y/%m/%d %H:%M:%S') + " : Saving " + str(resid - i) + " ...")
+            print(datetime.now().strftime("%H:%M:%S ") +
+                  "Saved " + str(resid - i))
             url = "https://app.splatoon2.nintendo.net/api/coop_results/" + \
                 str(resid - i)
-            res = requests.get(url, cookies=dict(iksm_session=self.session)).text
+            res = requests.get(url, cookies=dict(
+                iksm_session=self.cookie)).text
             path = os.path.dirname(os.path.abspath(
                 sys.argv[0])) + "/json/" + str(resid - i) + ".json"
             with open(path, mode="w") as f:
                 f.write(res)
-            sleep(5)
+            # sleep(5)
         return resid
+
 
 if __name__ == "__main__":
     user = SalmonRec()
-    
+    print(datetime.now().strftime("%H:%M:%S ") + "Waiting New Records.")
+
     while True:
         # 最新データを取得した上で、取得した最新のリザルトIDを保存
         latest = user.getResults()
-        user.uploadAll(latest)
-        sleep(10)
+        # user.uploadAll(latest)
+        sleep(5)
---
+++ iksm.py
@@ -0,0 +1,335 @@
+# eli fessler
+# clovervidia
+from __future__ import print_function
+from builtins import input
+import requests
+import json
+import re
+import sys
+import os
+import base64
+import hashlib
+import uuid
+import time
+import random
+import string
+
+session = requests.Session()
+version = "unknown"
+
+# place config.txt in same directory as script (bundled or not)
+if getattr(sys, 'frozen', False):
+    app_path = os.path.dirname(sys.executable)
+elif __file__:
+    app_path = os.path.dirname(__file__)
+config_path = os.path.join(app_path, "config.txt")
+
+
+def log_in(ver):
+    '''Logs in to a Nintendo Account and returns a session_token.'''
+
+    global version
+    version = ver
+
+    auth_state = base64.urlsafe_b64encode(os.urandom(36))
+
+    auth_code_verifier = base64.urlsafe_b64encode(os.urandom(32))
+    auth_cv_hash = hashlib.sha256()
+    auth_cv_hash.update(auth_code_verifier.replace(b"=", b""))
+    auth_code_challenge = base64.urlsafe_b64encode(auth_cv_hash.digest())
+
+    app_head = {
+        'Host':                      'accounts.nintendo.com',
+        'Connection':                'keep-alive',
+        'Cache-Control':             'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent':                'Mozilla/5.0 (Linux; Android 7.1.2; Pixel Build/NJH47D; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36',
+        'Accept':                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8n',
+        'DNT':                       '1',
+        'Accept-Encoding':           'gzip,deflate,br',
+    }
+
+    body = {
+        'state':                               auth_state,
+        'redirect_uri':                        'npf71b963c1b7b6d119://auth',
+        'client_id':                           '71b963c1b7b6d119',
+        'scope':                               'openid user user.birthday user.mii user.screenName',
+        'response_type':                       'session_token_code',
+        'session_token_code_challenge':        auth_code_challenge.replace(b"=", b""),
+        'session_token_code_challenge_method': 'S256',
+        'theme':                               'login_form'
+    }
+
+    url = 'https://accounts.nintendo.com/connect/1.0.0/authorize'
+    r = session.get(url, headers=app_head, params=body)
+
+    post_login = r.history[0].url
+
+    print("\nNavigate to this URL in your browser:")
+    print(post_login)
+    print("Log in, right click the \"Select this person\" button, copy the link address, and paste it below:")
+    while True:
+        try:
+            use_account_url = input("")
+            if use_account_url == "skip":
+                return "skip"
+            session_token_code = re.search('de=(.*)&', use_account_url)
+            return get_session_token(session_token_code.group(1), auth_code_verifier)
+        except KeyboardInterrupt:
+            print("\nBye!")
+            sys.exit(1)
+        except:
+            print("Malformed URL. Please try again, or press Ctrl+C to exit.")
+            print("URL:", end=' ')
+
+
+def get_session_token(session_token_code, auth_code_verifier):
+    '''Helper function for log_in().'''
+
+    app_head = {
+        'User-Agent':      'OnlineLounge/1.5.2 NASDKAPI Android',
+        'Accept-Language': 'en-US',
+        'Accept':          'application/json',
+        'Content-Type':    'application/x-www-form-urlencoded',
+        'Content-Length':  '540',
+        'Host':            'accounts.nintendo.com',
+        'Connection':      'Keep-Alive',
+        'Accept-Encoding': 'gzip'
+    }
+
+    body = {
+        'client_id':                   '71b963c1b7b6d119',
+        'session_token_code':          session_token_code,
+        'session_token_code_verifier': auth_code_verifier.replace(b"=", b"")
+    }
+
+    url = 'https://accounts.nintendo.com/connect/1.0.0/api/session_token'
+
+    r = session.post(url, headers=app_head, data=body)
+    return json.loads(r.text)["session_token"]
+
+
+def get_cookie(session_token, userLang, ver):
+
+    global version
+    version = ver
+
+    timestamp = int(time.time())
+    guid = str(uuid.uuid4())
+
+    app_head = {
+        'Host':            'accounts.nintendo.com',
+        'Accept-Encoding': 'gzip',
+        'Content-Type':    'application/json; charset=utf-8',
+        'Accept-Language': 'en-US',
+        'Content-Length':  '437',
+        'Accept':          'application/json',
+        'Connection':      'Keep-Alive',
+        'User-Agent':      'OnlineLounge/1.5.2 NASDKAPI Android'
+    }
+
+    body = {
+        'client_id':     '71b963c1b7b6d119',  # Splatoon 2 service
+        'session_token': session_token,
+        'grant_type':    'urn:ietf:params:oauth:grant-type:jwt-bearer-session-token'
+    }
+
+    url = "https://accounts.nintendo.com/connect/1.0.0/api/token"
+
+    r = requests.post(url, headers=app_head, json=body)
+    id_response = json.loads(r.text)
+
+    # get user info
+    try:
+        app_head = {
+            'User-Agent':      'OnlineLounge/1.5.2 NASDKAPI Android',
+            'Accept-Language': 'en-US',
+            'Accept':          'application/json',
+            'Authorization':   'Bearer {}'.format(id_response["access_token"]),
+            'Host':            'api.accounts.nintendo.com',
+            'Connection':      'Keep-Alive',
+            'Accept-Encoding': 'gzip'
+        }
+    except:
+        print("Not a valid authorization request. Please delete config.txt and try again.")
+        print("Error from Nintendo (in api/token step):")
+        print(json.dumps(id_response, indent=2))
+        sys.exit(1)
+    url = "https://api.accounts.nintendo.com/2.0.0/users/me"
+
+    r = requests.get(url, headers=app_head)
+    user_info = json.loads(r.text)
+
+    # nickname = user_info["nickname"]
+
+    # get access token
+    app_head = {
+        'Host':             'api-lp1.znc.srv.nintendo.net',
+        'Accept-Language':  'en-us',
+        'User-Agent':       'com.nintendo.znca/1.5.2 (Android/7.1.2)',
+        'Accept':           'application/json',
+        'X-ProductVersion': '1.5.2',
+        'Content-Type':     'application/json; charset=utf-8',
+        'Connection':       'Keep-Alive',
+        'Authorization':    'Bearer',
+        'Content-Length':   '1036',
+        'X-Platform':       'Android',
+        'Accept-Encoding':  'gzip'
+    }
+
+    body = {}
+
+    try:
+        idToken = id_response["id_token"]
+
+        flapg_response = call_flapg_api(idToken, guid, timestamp)
+        flapg_nso = flapg_response["login_nso"]
+        flapg_app = flapg_response["login_app"]
+
+        parameter = {
+            'f':          flapg_nso["f"],
+            'naIdToken':  flapg_nso["p1"],
+            'timestamp':  flapg_nso["p2"],
+            'requestId':  flapg_nso["p3"],
+            'naCountry':  user_info["country"],
+            'naBirthday': user_info["birthday"],
+            'language':   user_info["language"]
+        }
+    except SystemExit:
+        sys.exit(1)
+    except:
+        print("Error(s) from Nintendo:")
+        print(json.dumps(id_response, indent=2))
+        print(json.dumps(user_info, indent=2))
+        sys.exit(1)
+    body["parameter"] = parameter
+
+    url = "https://api-lp1.znc.srv.nintendo.net/v1/Account/Login"
+
+    r = requests.post(url, headers=app_head, json=body)
+
+    splatoon_token = json.loads(r.text)
+
+    # get splatoon access token
+    try:
+        app_head = {
+            'Host':             'api-lp1.znc.srv.nintendo.net',
+            'User-Agent':       'com.nintendo.znca/1.5.2 (Android/7.1.2)',
+            'Accept':           'application/json',
+            'X-ProductVersion': '1.5.2',
+            'Content-Type':     'application/json; charset=utf-8',
+            'Connection':       'Keep-Alive',
+            'Authorization':    'Bearer {}'.format(splatoon_token["result"]["webApiServerCredential"]["accessToken"]),
+            'Content-Length':   '37',
+            'X-Platform':       'Android',
+            'Accept-Encoding':  'gzip'
+        }
+    except:
+        print("Error from Nintendo (in Account/Login step):")
+        print(json.dumps(splatoon_token, indent=2))
+        sys.exit(1)
+
+    body = {}
+    parameter = {
+        'id':                5741031244955648,
+        'f':                 flapg_app["f"],
+        'registrationToken': flapg_app["p1"],
+        'timestamp':         flapg_app["p2"],
+        'requestId':         flapg_app["p3"]
+    }
+    body["parameter"] = parameter
+
+    url = "https://api-lp1.znc.srv.nintendo.net/v2/Game/GetWebServiceToken"
+
+    r = requests.post(url, headers=app_head, json=body)
+    splatoon_access_token = json.loads(r.text)
+
+    # get cookie
+    try:
+        app_head = {
+            'Host':                    'app.splatoon2.nintendo.net',
+            'X-IsAppAnalyticsOptedIn': 'false',
+            'Accept':                  'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Encoding':         'gzip,deflate',
+            'X-GameWebToken':          splatoon_access_token["result"]["accessToken"],
+            'Accept-Language':         'en-US',
+            'X-IsAnalyticsOptedIn':    'false',
+            'Connection':              'keep-alive',
+            'DNT':                     '0',
+            'User-Agent':              'Mozilla/5.0 (Linux; Android 7.1.2; Pixel Build/NJH47D; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36',
+            'X-Requested-With':        'com.nintendo.znca'
+        }
+    except:
+        print("Error from Nintendo (in Game/GetWebServiceToken step):")
+        print(json.dumps(splatoon_access_token, indent=2))
+        sys.exit(1)
+
+    url = "https://app.splatoon2.nintendo.net/?lang={}".format(userLang)
+    r = requests.get(url, headers=app_head)
+    return r.cookies["iksm_session"]
+
+
+def get_hash_from_s2s_api(id_token, timestamp):
+    '''Passes an id_token and timestamp to the s2s API and fetches the resultant hash from the response.'''
+
+    # check to make sure we're allowed to contact the API. stop spamming my web server pls
+    # config_file = open(config_path, "r")
+    # config_data = json.load(config_file)
+    # config_file.close()
+
+    # proceed normally
+    try:
+        api_app_head = {'User-Agent': "splatnet2statink/{}".format(version)}
+        api_body = {'naIdToken': id_token, 'timestamp': timestamp}
+        api_response = requests.post(
+            "https://elifessler.com/s2s/api/gen2", headers=api_app_head, data=api_body)
+        return json.loads(api_response.text)["hash"]
+    except:
+        print("Error from the splatnet2statink API:\n{}".format(
+            json.dumps(json.loads(api_response.text), indent=2)))
+
+        # add 1 to api_errors in config
+        config_file = open(config_path, "r")
+        config_data = json.load(config_file)
+        config_file.close()
+        try:
+            num_errors = config_data["api_errors"]
+        except:
+            num_errors = 0
+        num_errors += 1
+        config_data["api_errors"] = num_errors
+
+        config_file = open(config_path, "w")  # from write_config()
+        config_file.seek(0)
+        config_file.write(json.dumps(config_data, indent=4,
+                                     sort_keys=True, separators=(',', ': ')))
+        config_file.close()
+        sys.exit(1)
+
+
+def call_flapg_api(id_token, guid, timestamp):
+    '''Passes in headers to the flapg API (Android emulator) and fetches the response.'''
+
+    api_app_head = {
+        'x-token': id_token,
+        'x-time':  str(timestamp),
+        'x-guid':  guid,
+        'x-hash':  get_hash_from_s2s_api(id_token, timestamp),
+        'x-ver':   '2',
+        'x-iid':   ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
+    }
+    api_response = requests.get(
+        "https://flapg.com/ika2/api/login", headers=api_app_head)
+    f = json.loads(api_response.text)
+    return f
+
+
+def enter_cookie():
+    '''Prompts the user to enter their iksm_session cookie'''
+
+    new_cookie = input(
+        "Go to the page below to find instructions to obtain your iksm_session cookie:\nhttps://github.com/frozenpandaman/splatnet2statink/wiki/mitmproxy-instructions\nEnter it here: ")
+    while len(new_cookie) != 40:
+        new_cookie = input(
+            "Cookie is invalid. Please enter it again.\nCookie: ")
+    return new_cookie
