53c1a1cb2d5cb25d1626b9861f9bd6b2cbbebc03
tkgstrator <nasawake.am@gmail.com>
2021-01-12 04:47:10 +0900

    Update iksm.py

--- iksm.py
+++ iksm.py
@@ -78,7 +78,7 @@ def get_session_token(session_token_code, auth_code_verifier):
     '''Helper function for log_in().'''
 
     app_head = {
-        'User-Agent':      'OnlineLounge/1.9.0 NASDKAPI Android',
+        'User-Agent':      'OnlineLounge/1.10.0 NASDKAPI Android',
         'Accept-Language': 'en-US',
         'Accept':          'application/json',
         'Content-Type':    'application/x-www-form-urlencoded',
@@ -117,7 +117,7 @@ def get_cookie(session_token, userLang, ver):
         'Content-Length':  '439',
         'Accept':          'application/json',
         'Connection':      'Keep-Alive',
-        'User-Agent':      'OnlineLounge/1.9.0 NASDKAPI Android'
+        'User-Agent':      'OnlineLounge/1.10.0 NASDKAPI Android'
     }
 
     body = {
@@ -134,7 +134,7 @@ def get_cookie(session_token, userLang, ver):
     # get user info
     try:
         app_head = {
-            'User-Agent':      'OnlineLounge/1.9.0 NASDKAPI Android',
+            'User-Agent':      'OnlineLounge/1.10.0 NASDKAPI Android',
             'Accept-Language': userLang,
             'Accept':          'application/json',
             'Authorization':   'Bearer {}'.format(id_response["access_token"]),
@@ -156,9 +156,9 @@ def get_cookie(session_token, userLang, ver):
     app_head = {
         'Host':             'api-lp1.znc.srv.nintendo.net',
         'Accept-Language':  userLang,
-        'User-Agent':       'com.nintendo.znca/1.9.0 (Android/7.1.2)',
+        'User-Agent':       'com.nintendo.znca/1.10.0 (Android/7.1.2)',
         'Accept':           'application/json',
-        'X-ProductVersion': '1.9.0',
+        'X-ProductVersion': '1.10.0',
         'Content-Type':     'application/json; charset=utf-8',
         'Connection':       'Keep-Alive',
         'Authorization':    'Bearer',
@@ -207,9 +207,9 @@ def get_cookie(session_token, userLang, ver):
     try:
         app_head = {
             'Host':             'api-lp1.znc.srv.nintendo.net',
-            'User-Agent':       'com.nintendo.znca/1.9.0 (Android/7.1.2)',
+            'User-Agent':       'com.nintendo.znca/1.10.0 (Android/7.1.2)',
             'Accept':           'application/json',
-            'X-ProductVersion': '1.9.0',
+            'X-ProductVersion': '1.10.0',
             'Content-Type':     'application/json; charset=utf-8',
             'Connection':       'Keep-Alive',
             'Authorization':    'Bearer {}'.format(splatoon_token["result"]["webApiServerCredential"]["accessToken"]),
@@ -315,5 +315,5 @@ def enter_cookie():
 
     new_cookie = input("Go to the page below to find instructions to obtain your iksm_session cookie:\nhttps://github.com/frozenpandaman/splatnet2statink/wiki/mitmproxy-instructions\nEnter it here: ")
     while len(new_cookie) != 40:
-        new_cookie = input("Cookie is invalid. Please enter it again.\nCookie: ")
+        newcookie = input("Cookie is invalid. Please enter it again.\nCookie: ")
     return new_cookie
